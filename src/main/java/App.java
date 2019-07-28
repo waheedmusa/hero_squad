715bea2b7f9bb8121310880e7579634017992381
waheedmusa <abdulwaheedmusa0@gmail.com>
2019-07-28 16:42:34 +0300

    form updated

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,7 +10,12 @@ public class App {
         staticFileLocation("/public");
         get("/", (request, response) -> { //request for route happens at this location
             Map<String, Object> model = new HashMap<String, Object>(); // new model is made to store information
-            return new ModelAndView(model, "hello.hbs"); // assemble individual pieces and render
+            return new ModelAndView(model, "hero-form.hbs"); // assemble individual pieces and render
+        }, new HandlebarsTemplateEngine());
+
+        get("/squad-form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "squad-form.hbs");
         }, new HandlebarsTemplateEngine());
     }
 
