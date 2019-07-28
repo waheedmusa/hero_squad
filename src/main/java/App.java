0089fcb234292409a311bc45b486d934944811a5
waheedmusa <abdulwaheedmusa0@gmail.com>
2019-07-28 16:35:57 +0300

    form created

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args){
+        staticFileLocation("/public");
+        get("/", (request, response) -> { //request for route happens at this location
+            Map<String, Object> model = new HashMap<String, Object>(); // new model is made to store information
+            return new ModelAndView(model, "hello.hbs"); // assemble individual pieces and render
+        }, new HandlebarsTemplateEngine());
+    }
 
 }
