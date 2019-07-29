10c9c2a929fd874940637afef98d0cdf96454d83
waheedmusa <abdulwaheedmusa0@gmail.com>
2019-07-29 12:24:03 +0300

    changes made

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,9 @@
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import spark.ModelAndView;
+
 import java.util.HashMap;
 import java.util.Map;
 
-import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
@@ -13,6 +14,27 @@ public class App {
             Map<String, Object> model = new HashMap<String, Object>(); // new model is made to store information
             return new ModelAndView(model, "home.hbs"); // assemble individual pieces and render
         }, new HandlebarsTemplateEngine()); //
+
+        get("/hero_form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "hero-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/squad_form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String name = request.queryParams("name");
+            model.put("name", name);
+            return new ModelAndView(model, "squad-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/hero_squad", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String squad = request.queryParams("squad");
+            model.put("squad", squad);
+            return new ModelAndView(model, "hero-squad.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
     }
 
 }
--- src/test/java/SquadTest.groovy
+++ src/test/java/SquadTest.groovy
@@ -1,2 +1,78 @@
-class SquadTest extends groovy.util.GroovyTestCase {
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SquadTest {
+    @Test
+    public void creates_instanceOfSquad() {
+        Squad squad = new Squad(7, "Squad A", "fight crime");
+        assertTrue(squad instanceof Squad);
+    }
+
+    @Test
+    public void saves_Size_7()
+    {
+        Squad squad = new Squad(7,"Squad A","fight crime");
+        assertEquals(5, squad.getSize());
+    }
+
+    @Test
+    public void saves_squadName_squad_A()
+    {
+        Squad squad = new Squad(7,"Squad A","fight crime");
+        assertEquals("Squad A", squad.getName());
+    }
+
+    @Test
+    public void saves_fight_crime()
+    {
+        Squad squad = new Squad(7,"Squad A","fight crime");
+        assertEquals("fight crime", squad.getCause());
+    }
+
+    @Test
+    public void all_returns_all_instancesOfSquad()
+    {
+        Squad squad1 = new Squad(7,"Squad A","fight crime");
+        Squad squad2 = new Squad(7,"Squad A","fight crime");
+        assertTrue(Squad.all().contains(squad1));
+        assertTrue(Squad.all().contains(squad2));
+    }
+
+    @Test
+    public void clear_emptiesAllSquadsFromList_0() {
+        Squad.clear();
+        assertEquals(Squad.all().size(), 0);
+    }
+
+    @Test
+    public void getId_SquadInstantiateWithAnId_1() {
+        Squad.clear();
+        Squad squad = new Squad(7,"Squad A","fight crime");
+        assertEquals(1,squad.getId());
+    }
+
+    @Test
+    public void find_returnsCategoryWithSameId_secondCategory() {
+        Squad.clear();
+        Squad squad = new Squad(7,"Squad A","fight crime");
+        assertEquals(Squad.find(squad.getId()), squad);
+    }
+
+    @Test
+    public void getSquad_initiallyReturnsEmptyList_ArrayList() {
+        Squad.clear();
+        Squad squad = new Squad(7,"Squad A","fight crime");
+        assertEquals(0, squad.getHeroes().size());
+    }
+
+    @Test
+    public void addsHeroesToList_true() {
+        Squad squad = new Squad(7,"Squad A","fight crime");
+        Hero hero = new Hero("Batman",25,"Rich","None");
+        squad.addHero(hero);
+        assertTrue(squad.getHeroes().contains(hero));
+    }
+
+
 }
